1001fa2cfe59660454f9fd84319487c317274eb4
reuben_lalhmunsiama <reubenlalhmunsiama@gmail.com>
2021-07-30 02:59:10 +0530

    corrected

--- routes/web.php
+++ routes/web.php
@@ -12,10 +12,6 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/test', function(){
-  return 'This is test';
-});
-
 Route::get('/', function () {
     return view('welcome');
 });
